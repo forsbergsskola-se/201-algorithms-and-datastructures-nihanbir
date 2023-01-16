b6fc3d444f5ca14202dceb3de382a390441865fb
nihanbir <nihan.bir@gmail.com>
2023-01-16 13:38:01 +0100

    #0 - Completes Introduction exercise

---
+++ P201AlgorithmsAndDataStructures/TurboCollections.Test/MathsTests.cs
@@ -0,0 +1,11 @@
+namespace TurboCollections.Test;
+
+public static class MathsTests
+{
+ [Test]
+    public static void SayHelloExists()
+    {
+        TurboMaths.SayHello();
+        Assert.Pass();
+    }
+}
