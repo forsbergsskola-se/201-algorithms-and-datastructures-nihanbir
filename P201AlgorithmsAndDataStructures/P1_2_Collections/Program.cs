1cab19409806976c1a83b0520292f57bffb4d718
nihanbir <nihan.bir@gmail.com>
2023-01-16 13:47:27 +0100

    #1 - Completes Collections exercise

---
+++ P201AlgorithmsAndDataStructures/P1_2_Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+List<int> numbers = new() {
+    137,
+    1000,
+    -200
+};
+
+for (var i = 0; i < numbers.Count; i++) {
+    Console.WriteLine(numbers[i]);
+}
+
+ArrayList list = new() {
+    true,
+    "Forsbergs",
+    'a',
+    1000,
+    .12f
+};
+
+for (var i = 0; i < list.Count; i++) {
+    Console.WriteLine(list[i]);
+}
