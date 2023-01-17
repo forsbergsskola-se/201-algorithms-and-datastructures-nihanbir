3785336b688c6efdae85de678d7881b23525c4e1
nihanbir <nihan.bir@gmail.com>
2023-01-17 14:55:44 +0100

    #4 Completes 4.2 Class exercise

---
+++ P201AlgorithmsAndDataStructures/TurboCollections/TurboLinkedStack.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+
+namespace TurboCollections;
+
+public class TurboLinkedStack<T> : IEnumerable<T> {
+    class Node {
+        public T? Value;
+        public Node? Previous;
+    }
+    Node? LastNode;
+
+    public void Push(T item)
+    {
+        // Insert Code from AddNumber Example in #4 here
+        LastNode = new Node {
+            Value = item,
+            Previous = LastNode
+        };
+    }
+
+    public T Peek() {
+        // Return the Value of Last Node here.
+        return LastNode.Value;
+    }
+
+    public T Pop()
+    {
+        // 1. Save the Last Node locally so we can return the value later.
+        Node? node = LastNode;
+        // 2. Now, assign the Last Node's Previous Node to be the Last Node.
+        if (LastNode != null) LastNode = LastNode.Previous;
+        // -- This effectively removes the previously Last Node of the Stack
+        // -- Imagine LastNode is customer 436
+        // -- -- who remembered that customer 435 was before him.
+        // -- We assign that before customer 435 to LastNode.
+        // -- -- 435 knows that 434 was before him.
+        // -- -- But he has no memory of customer 436.
+        // Now, return the Value of the Node that you cached in Step 1.
+        return node.Value;
+    }
+
+    public void Clear() {
+        // This one is incredibly easy. Just assign null to Field LastNode
+        LastNode = null;
+        // -- This is like pretending you never new that there is any last customer.
+        // -- by forgetting the latest customer, you forget them all.
+    }
+
+    public int Count {
+        get{
+            // Here, you need to do a while loop over all nodes
+            // Similar to the previous PrintAllNodes Function
+            // But instead of Printing Nodes, you just count how many Nodes you have visited
+            // Similar to this:
+            int count = 0;
+            Node? lastNode = LastNode;
+            while(LastNode != null/* remove false and replace with correct condition...*/)
+            {
+                count++;
+                lastNode = lastNode.Previous;
+            }
+            return count;
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator() {
+        // This one is a bonus and a bit more difficult.
+        // You need to create a new class named Enumerator.
+        // You find the details below.
+        var enumerator = new Enumerator{
+            CurrentNode = null,
+            // This might look confusing. But remember? Last In. First Out.
+            FirstNode = LastNode
+        };
+        return enumerator;
+    }
+    
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    class Enumerator : IEnumerator<T> {
+        public Node? CurrentNode;
+        public Node? FirstNode;
+
+        public bool MoveNext(){
+            // if we don't have a current node, we start with the first node
+            if(CurrentNode == null) CurrentNode = FirstNode; 
+            // Assign the Current Node's Previous Node to be the Current Node.
+            else CurrentNode = CurrentNode.Previous;
+            // Return, whether there is a CurrentNode. Else, we have reached the end of the Stack, there's no more Elements.
+            return CurrentNode != null;
+        }
+        
+        // Return the Current Node's Value.
+        public T Current => CurrentNode.Value;
+
+        // This Boiler Plate is necessary to correctly implement `IEnumerable` interface.
+        object IEnumerator.Current => Current;
+
+        public void Reset() {
+            // Look at Move. How can you make sure that this Enumerator starts over again?
+            CurrentNode = FirstNode;
+        }
+
+        public void Dispose()
+        {
+            // This function is not needed right now.
+        }
+    }
+}
