c2b26faa01c77f891778b14f137cffb1c974d11f
nihanbir <nihan.bir@gmail.com>
2023-01-20 12:57:28 +0100

    #5 Completes 5.3 SpotifySongQueue application

---
+++ P201AlgorithmsAndDataStructures/SpotifySongQueue/Program.cs
@@ -0,0 +1,23 @@
+﻿using TurboCollections;
+
+TurboLinkedQueue<string> songList = new TurboLinkedQueue<string>();
+while (true) 
+{
+    Console.WriteLine("What would you like to do? [s]kip or [a]dd?");
+    
+    var input = Console.ReadKey().Key;
+    
+    Console.WriteLine();
+    
+    if (input == ConsoleKey.A) 
+    {
+        Console.WriteLine("Enter the Song's Name");
+        songList.Enqueue(Console.ReadLine() ?? string.Empty);
+    }
+    if (input == ConsoleKey.S)
+    {
+        Console.WriteLine(songList.Count == 0
+            ? "There is no more songs in the Queue."
+            : $"Now Playing: {songList.Dequeue()}");
+    }
+}
