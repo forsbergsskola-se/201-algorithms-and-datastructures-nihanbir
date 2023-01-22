4fddc0e3ae926ebe42f452fb9c24c2f8d253d621
nihanbir <nihan.bir@gmail.com>
2023-01-22 21:05:11 +0100

    #5 Refactors Enqueue and Dequeue

--- P201AlgorithmsAndDataStructures/TurboCollections.Test/TurboQueueTests.cs
+++ P201AlgorithmsAndDataStructures/TurboCollections.Test/TurboQueueTests.cs
@@ -3,25 +3,43 @@ namespace TurboCollections.Test;
 public class TurboQueueTests
 {
     [Test]
-    [TestCase(2,4)]
+    [TestCase(2, 4)]
     [TestCase("sup", "wassup")]
-    [TestCase('a','b')]
-    public static void TurboQueueWorks(object a, object b)
+    [TestCase('a', 'b')]
+    public static void EnqueueWorks(object a, object b)
     {
         var list = new TurboQueue<object>();
         list.Enqueue(a);
         list.Enqueue(b);
         Assert.That(list.Peek(), Is.EqualTo(a));
         Assert.That(list.Count, Is.EqualTo(2));
-        
+    }
+    [Test]
+    [TestCase(2, 4)]
+    [TestCase("sup", "wassup")]
+    [TestCase('a', 'b')]
+    public static void DequeueWorks(object a, object b)
+    {
+        var list = new TurboQueue<object>();
+        list.Enqueue(a);
         Assert.That(list.Dequeue(), Is.EqualTo(a));
-        Assert.That(list.Peek(), Is.EqualTo(b));
-        
+        Assert.Throws<NullReferenceException>(() => list.Dequeue());
+    }
+    
+    [Test]
+    [TestCase(2, 4)]
+    [TestCase("sup", "wassup")]
+    [TestCase('a', 'b')]
+    public static void ClearWorks(object a, object b)
+    {
+        var list = new TurboQueue<object>();
         list.Enqueue(b);
         list.Clear();
         Assert.That(list.Count, Is.EqualTo(0));
     }
-    
+   
+   
+
     //Ale's tests, also exists in TurboLinkedStackTests
     [Test]
     public void MoveNextReturnsTrueWhenListIsNotEmpty()
--- P201AlgorithmsAndDataStructures/TurboCollections/TurboQueue.cs
+++ P201AlgorithmsAndDataStructures/TurboCollections/TurboQueue.cs
@@ -4,19 +4,23 @@ namespace TurboCollections;
 
 public class TurboQueue<T> : ITurboQueue<T>
 {
-    public int Count => Array.Length;
+    public int Count { get; set; }
 
-    protected T[] Array = System.Array.Empty<T>();
+    private T[] Array = System.Array.Empty<T>();
     
     public void Enqueue(T item)
     {
-        T[] newArray = new T[Array.Length + 1];
-        newArray[^1] = item;
-        for (int i = 0; i < Array.Length; i++)
+        if (Count == Array.Length)
         {
-            newArray[i] = Array[i];
+            T[] newArray = new T[Math.Max(Array.Length * 2, 1)];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                newArray[i] = Array[i];
+            }
+            Array = newArray;
         }
-        Array = newArray;
+        Array[Count] = item;
+        Count++;
     }
 
     public T Peek()
@@ -26,19 +30,20 @@ public class TurboQueue<T> : ITurboQueue<T>
 
     public T Dequeue()
     {
+        if (Count == 0) throw new NullReferenceException();
         T firstIndex = Array[0];
-        T[] newArray = new T[Array.Length-1];
-        for (int i = 0; i < newArray.Length; i++)
+        Count--;
+        for (int i = 0; i < Count; i++)
         {
-            newArray[i] = Array[i+1];
+            Array[i] = Array[i+1];
         }
-        Array = newArray;
         return firstIndex;
     }
 
     public void Clear()
     {
         Array = System.Array.Empty<T>();
+        Count = 0;
     }
     
     public IEnumerator<T> GetEnumerator()
