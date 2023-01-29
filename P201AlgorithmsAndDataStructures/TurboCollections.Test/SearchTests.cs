3dae1f95eff2a5128b6eb33326beb7317a2572a3
nihanbir <nihan.bir@gmail.com>
2023-01-29 22:25:49 +0100

    Update SearchTests.cs

--- P201AlgorithmsAndDataStructures/TurboCollections.Test/SearchTests.cs
+++ P201AlgorithmsAndDataStructures/TurboCollections.Test/SearchTests.cs
@@ -18,36 +18,37 @@ public class SearchTests
         Assert.That(TurboSearch.LinearSearch(list, 1), Is.EqualTo(-1));
         Assert.That(TurboSearch.LinearSearch(list, 89), Is.EqualTo(4));
     }
-    
+
     [Test]
-    public void LinearSearchs()
+    [TestCase(2)]
+    [TestCase(7)]
+    [TestCase(89)]
+    public void SearchComparison(int value)
     {
-        var list = new TurboLinkedList<int>{2,5,7,8,89};
-        
+        var list = new TurboLinkedList<int> { 2, 5, 7, 8, 89 };
+
         var timer = new Stopwatch();
         timer.Start();
-        TurboSearch.LinearSearch(list, 89);
+        TurboSearch.LinearSearch(list, value);
         timer.Stop();
-        var linearTimer= timer.Elapsed;
-        Console.WriteLine($"Linear Search: {timer.Elapsed}");
-        
-        timer.Restart();
-        TurboSearch.BinarySearch(list, 89);
-        timer.Stop();
-        var binaryTimer= timer.Elapsed;
-        Console.WriteLine($"Binary Search: {timer.Elapsed}");
+        var linearTimer = timer.Elapsed;
+        Console.WriteLine($"Linear Search: {linearTimer}");
 
+        timer.Restart();
+        TurboSearch.BinarySearch(list, value);
+        timer.Stop();
+        var binaryTimer = timer.Elapsed;
+        Console.WriteLine($"Binary Search: {binaryTimer}");
+        
         if (binaryTimer < linearTimer)
         {
-            var comparisonResult = (linearTimer - binaryTimer) / linearTimer * 100;
-            Console.WriteLine($"Binary Search was {(int)comparisonResult}% faster than Linear Search");
+            var comparisonResult = linearTimer / binaryTimer;
+            Console.WriteLine($"Binary Search was {Math.Round(comparisonResult, 1)} times faster");
         }
         else
         {
-            var comparisonResult = (binaryTimer - linearTimer) / binaryTimer * 100;
-            Console.WriteLine($"Linear Search was {(int)comparisonResult}% faster than Binary Search");
+            var comparisonResult = binaryTimer / linearTimer;
+            Console.WriteLine($"Linear Search was {Math.Round(comparisonResult, 1)} times faster");
         }
-        
     }
-    
 }
