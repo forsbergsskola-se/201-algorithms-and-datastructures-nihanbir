be2fc22d8149aba39bf080e9bb03414597998189
nihanbir <nihan.bir@gmail.com>
2023-01-30 10:11:34 +0100

    Update SearchTests.cs

--- P201AlgorithmsAndDataStructures/TurboCollections/LinearSearch.cs
+++ P201AlgorithmsAndDataStructures/TurboCollections/LinearSearch.cs
@@ -4,15 +4,12 @@ public static partial class TurboSearch
 {
     public static int LinearSearch(TurboLinkedList<int> list, int value)
     {
+        var count = 0;
         foreach (var i in list)
         {
-            if (i == value) return i;
+            if (list[i] == value) return count;
+            count++;
         }
-        
-        // for (var i = 0; i < list.Count; i++)
-        // {
-        //     if (list[i] == value) return i;
-        // }
         return -1;
     }
 }
