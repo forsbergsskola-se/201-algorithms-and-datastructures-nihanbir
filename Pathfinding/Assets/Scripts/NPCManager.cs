60c2452e9976df8b7a8cc44c98ab0befcb59509f
nihanbir <nihan.bir@gmail.com>
2023-02-23 15:45:30 +0300

    Uploads assets

---
+++ Pathfinding/Assets/Scripts/NPCManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        throw new NotImplementedException();
+    }
+}
